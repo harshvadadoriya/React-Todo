ca5459bd2a0f0a6413c31a182e3c373cf9cb1458
Harsh Vadadoriya <harshvadadoriya079@gmail.com>
2023-04-25 00:18:21 +0530

    practical-3 logic built

--- src/components/TodoContainer.tsx
+++ src/components/TodoContainer.tsx
@@ -1,11 +1,90 @@
 import React, { useState, useRef, useEffect } from 'react';
 import DateComponent from './DateComponent';
 import TodoComponent from './TodoListComponent';
+import TaskCompletedImg from '../assets/transparent/completing-tasks-unscreen.gif';
+
+type Todo = {
+	id: string;
+	text: string;
+	status: 'pending' | 'completed';
+};
 
 const TodoContainer: React.FC = (): JSX.Element => {
+	const [todos, setTodos] = useState<Todo[]>([]);
+	const [completedTodos, setCompletedTodos] = useState<Todo[]>([]);
 	const [showInput, setShowInput] = useState(false);
 	const inputRef: React.RefObject<HTMLInputElement> =
 		useRef<HTMLInputElement>(null);
+
+	useEffect(() => {
+		const storedTodos = JSON.parse(localStorage.getItem('todos') || '[]');
+		const storedCompletedTodos = JSON.parse(
+			localStorage.getItem('completedTodos') || '[]'
+		);
+		setTodos(storedTodos);
+		setCompletedTodos(storedCompletedTodos);
+
+		const currentDate = new Date().toLocaleDateString();
+		const storedDate = localStorage.getItem('date');
+
+		if (currentDate !== storedDate) {
+			localStorage.removeItem('todos');
+			localStorage.removeItem('completedTodos');
+			setTodos([]);
+			setCompletedTodos([]);
+		}
+		localStorage.setItem('date', currentDate);
+	}, []);
+
+	const handleAddTodo = (todo: Todo) => {
+		const updatedTodos = [...todos, todo];
+		setTodos(updatedTodos);
+		localStorage.setItem('todos', JSON.stringify(updatedTodos));
+	};
+
+	const handleRemoveTodo = (id: string) => {
+		const updatedTodos = todos.filter((t) => t.id !== id);
+		const updatedCompletedTodos = completedTodos.filter((t) => t.id !== id);
+		setTodos(updatedTodos);
+		setCompletedTodos(updatedCompletedTodos);
+		localStorage.setItem('todos', JSON.stringify(updatedTodos));
+		localStorage.setItem(
+			'completedTodos',
+			JSON.stringify(updatedCompletedTodos)
+		);
+	};
+
+	const handleToggleTodo = (todo: Todo) => {
+		const updatedTodos = [...todos];
+		const updatedCompletedTodos = [...completedTodos];
+
+		const todoIndex = updatedTodos.findIndex((t) => t.id === todo.id);
+		const completedTodoIndex = updatedCompletedTodos.findIndex(
+			(t) => t.id === todo.id
+		);
+
+		if (todoIndex !== -1 && completedTodoIndex === -1) {
+			// update the status of the clicked todo to "completed"
+			updatedTodos[todoIndex].status = 'completed';
+			updatedCompletedTodos.push(updatedTodos[todoIndex]);
+			updatedTodos.splice(todoIndex, 1);
+		}
+
+		// else if (todoIndex === -1 && completedTodoIndex !== -1) {
+		// 	// update the status of the clicked todo back to "pending"
+		// 	updatedCompletedTodos[completedTodoIndex].status = 'pending';
+		// 	updatedTodos.push(updatedCompletedTodos[completedTodoIndex]);
+		// 	updatedCompletedTodos.splice(completedTodoIndex, 1);
+		// }
+
+		setTodos(updatedTodos);
+		setCompletedTodos(updatedCompletedTodos);
+		localStorage.setItem('todos', JSON.stringify(updatedTodos));
+		localStorage.setItem(
+			'completedTodos',
+			JSON.stringify(updatedCompletedTodos)
+		);
+	};
 
 	const handlePlusClick = (): void => {
 		setShowInput(true);
@@ -34,27 +113,68 @@ const TodoContainer: React.FC = (): JSX.Element => {
 				<div className="mx-4 card">
 					<div className="container">
 						<DateComponent />
-						<div className="todo-container">
-							<TodoComponent text="Go for a walk" status="completed" />
-							<TodoComponent text="Try not to fall asleep" status="completed" />
-							<TodoComponent text="Go To Gym" status="pending" />
-							<TodoComponent text="Read an article" status="pending" />
-							<TodoComponent text="Watch 'Sherlock'" status="pending" />
-						</div>
+						{todos.length === 0 && completedTodos.length === 0 ? (
+							<div className="todo-container task-completed">
+								<img
+									className="img-fluid"
+									src={TaskCompletedImg}
+									alt="All Task Completed"
+								/>
+							</div>
+						) : (
+							<div className="todo-container">
+								{todos.map((todo) => (
+									<TodoComponent
+										key={todo.id}
+										text={todo.text}
+										status="pending"
+										onToggle={() => handleToggleTodo(todo)}
+										onRemove={() => handleRemoveTodo(todo.id)}
+									/>
+								))}
+								{completedTodos.map((todo) => (
+									<TodoComponent
+										key={todo.id}
+										text={todo.text}
+										status="completed"
+										onToggle={() => handleToggleTodo(todo)}
+										onRemove={() => handleRemoveTodo(todo.id)}
+									/>
+								))}
+							</div>
+						)}
+
 						{showInput ? (
 							<div className="inputText">
 								<input
 									type="text"
 									placeholder="Add Your Todo!"
 									ref={inputRef}
+									onKeyDown={(event) => {
+										if (event.key === 'Enter') {
+											const todoInput = event.target as HTMLInputElement;
+											if (todoInput.value.trim() !== '') {
+												const todo: Todo = {
+													id: Date.now().toString(),
+													text: todoInput.value.trim(),
+													status: 'pending',
+												};
+												handleAddTodo(todo);
+												todoInput.value = '';
+												setShowInput(false);
+											}
+										}
+									}}
 								/>
 							</div>
 						) : (
-							<button className="plus" onClick={handlePlusClick}>
-								<div className="add">
-									<p>+</p>
-								</div>
-							</button>
+							<div className="add-todo">
+								<button className="plus" onClick={handlePlusClick}>
+									<div className="add">
+										<p>+</p>
+									</div>
+								</button>
+							</div>
 						)}
 					</div>
 				</div>
--- src/components/TodoListComponent.tsx
+++ src/components/TodoListComponent.tsx
@@ -5,10 +5,15 @@ import successTodoImg from '../assets/task-completed.png';
 type TodoProps = {
 	text: string;
 	status: 'completed' | 'pending';
+	onToggle: () => void;
+	onRemove: () => void;
 };
 
 // React.Component used to define Class Component and it takes in the TodoProps interface as its generic type parameter.
 class TodoComponent extends React.Component<TodoProps> {
+	handleToggle = (): void => {
+		this.props.onToggle();
+	};
 	render(): React.ReactNode {
 		const { text, status } = this.props;
 		return (
@@ -21,6 +26,7 @@ class TodoComponent extends React.Component<TodoProps> {
 						className="todo-status"
 						src={status === 'completed' ? successTodoImg : pendingTodoImg}
 						alt={status === 'completed' ? 'completed' : 'pending'}
+						onClick={this.handleToggle}
 					/>
 				</div>
 			</div>
